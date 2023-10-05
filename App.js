d0ee390062893fc2dee17746eaf8a5f7df426e9c
Jagerziel <fromnytosg@gmail.com>
2023-10-05 19:26:48 -0400

    removed headbar for navigating screens

--- App.js
+++ App.js
@@ -27,18 +27,21 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-        <Stack.Navigator initialRouteName='HomeScreen'>
-          <Stack.Screen name="HomeScreen" component={HomeScreen}/>
-          <Stack.Screen name="LearningScreen" component={LearningScreen}/>
-          <Stack.Screen name="ChatScreen" component={ChatScreen}/>
-          <Stack.Screen name="AboutUsScreen" component={AboutUsScreen}/>
-          <Stack.Screen name="SettingsScreen" component={SettingsScreen}/>
-          <Stack.Screen name="LoginScreen" component={LoginScreen}/>
-          <Stack.Screen name="UpdatePetScreen" component={UpdatePetScreen}/>
-          <Stack.Screen name="UserProfileScreen" component={UserProfileScreen}/>
-          <Stack.Screen name="VaccinationScreen" component={VaccinationScreen}/>
-        </Stack.Navigator>
-      </NavigationContainer>
+      <Stack.Navigator 
+        initialRouteName='HomeScreen'
+        screenOptions={{ headerShown: false }}
+      >
+        <Stack.Screen name="HomeScreen" component={HomeScreen}/>
+        <Stack.Screen name="LearningScreen" component={LearningScreen}/>
+        <Stack.Screen name="ChatScreen" component={ChatScreen}/>
+        <Stack.Screen name="AboutUsScreen" component={AboutUsScreen}/>
+        <Stack.Screen name="SettingsScreen" component={SettingsScreen}/>
+        <Stack.Screen name="LoginScreen" component={LoginScreen}/>
+        <Stack.Screen name="UpdatePetScreen" component={UpdatePetScreen}/>
+        <Stack.Screen name="UserProfileScreen" component={UserProfileScreen}/>
+        <Stack.Screen name="VaccinationScreen" component={VaccinationScreen}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,28 +2,29 @@ import React from 'react';
 import { View , Text , StyleSheet, Button } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
-
 function Navigation(  ) {
     const navigation = useNavigation(); 
+    
     return (
         <View style={styles.container}>
-           <Button
+           <Button style={styles.button}
                 title="Home"
                 onPress={() => navigation.navigate('HomeScreen')}
             />    
-            <Button
+            <Button style={styles.button}
                 title="Learning"
-                onPress={() => navigation.navigate('LearningScreen')}
+                onPress={() => (navigation.navigate('LearningScreen'))}
             /> 
-            <Button
+            <Button style={styles.button}
                 title="Chat"
                 onPress={() => navigation.navigate('ChatScreen')}
             /> 
-            <Button
+            <Button style={styles.button}
                 title="AboutUs"
                 onPress={() => navigation.navigate('AboutUsScreen')}
+                
             /> 
-            <Button
+            <Button style={styles.button}
                 title="Settings"
                 onPress={() => navigation.navigate('SettingsScreen')}
             /> 
@@ -39,6 +40,10 @@ const styles = StyleSheet.create({
       backgroundColor: '#eee',
       alignItems: 'center',
       justifyContent: 'center',
+      flexDirection: 'row'
     },
+    button: {
+        width: '10%'
+    }
 });
   
