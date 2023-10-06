6dbf5b0f3b6a5dab6e5a81e7f213871b61d35a75
Jagerziel <fromnytosg@gmail.com>
2023-10-05 20:42:33 -0400

    Updated nav bar styling

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,45 +1,55 @@
 import React from 'react';
 import { View , Text , StyleSheet, TouchableOpacity } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation, useRoute } from '@react-navigation/native';
 
 // Icons
 import { AntDesign, Entypo, Feather, Ionicons } from '@expo/vector-icons' //Home
 
+// Colors
+import { colors } from '../constants/colorPalette.js';
+
 
 function Navigation(  ) {
-    const navigation = useNavigation(); 
-    
+    const navigation = useNavigation()
+    const route = useRoute() 
+
+
+    // Dynamically render color of icon on nav bar
+    function iconColor (name) {
+        if (route.name === name) return colors.primary
+        return colors.grayscale03
+    }
     return (
         <View style={styles.container}>
             <TouchableOpacity 
                 style={styles.button}
                 onPress={() => navigation.navigate('HomeScreen')}
             >
-                <AntDesign name="home" size={24} color="black" />
+                <AntDesign name="home" size={24} color={iconColor('HomeScreen')} />
             </TouchableOpacity>
             <TouchableOpacity 
                 style={styles.button}
                 onPress={() => navigation.navigate('LearningScreen')}
             >
-                <Ionicons name="school-outline" size={26} color="black" />
+                <Ionicons name="school-outline" size={26} color={iconColor('LearningScreen')} />
             </TouchableOpacity>
             <TouchableOpacity 
                 style={styles.button}
                 onPress={() => navigation.navigate('ChatScreen')}
             >
-                <Ionicons name="chatbox-outline" size={22} color="black" />
+                <Ionicons name="chatbox-outline" size={22} color={iconColor('ChatScreen')} />
             </TouchableOpacity>
             <TouchableOpacity 
                 style={styles.button}
                 onPress={() => navigation.navigate('AboutUsScreen')}
             >
-                <Entypo name="star" size={27} color="black" />
+                <Entypo name="star" size={27} color={iconColor('AboutUsScreen')} />
             </TouchableOpacity>
             <TouchableOpacity 
                 style={styles.button}
                 onPress={() => navigation.navigate('SettingsScreen')}
             >
-                <Feather name="settings" size={24} color="black" />
+                <Feather name="settings" size={24} color={iconColor('SettingsScreen')} />
             </TouchableOpacity>
         </View>
     );
@@ -51,7 +61,7 @@ const styles = StyleSheet.create({
     container: {
         // flex: 1,
         height: 70,
-        backgroundColor: '#999',
+        backgroundColor: colors.white,
         alignItems: 'center',
         justifyContent: 'center',
         flexDirection: 'row',
--- src/constants/colorPalette.js
+++ src/constants/colorPalette.js
@@ -1,20 +1,16 @@
-// Primary and Secondary Colors
+// Primary and Secondary Colors plus Others for App
 export const colors = {
     primary: '#3E43BE',
     primaryFade:'#9598D9',
     secondary: '#F0900C',
     secondaryFade: '#F5CB91',
-    darkBlue: '#2E3150'
-}
-
-// Grayscale Colors
-export const grayScaleColors = {
+    darkBlue: '#2E3150',
     black: '#000000',
-    scale01: '#2E3150',
-    scale02: '#727272',
-    scale03: '#ACACAC',
-    scale04: '#E2E2E2',
-    scale05: '#F5F5F5',
-    scale06: '#F9F9F9',
+    grayscale01: '#2E3150',
+    grayscale02: '#727272',
+    grayscale03: '#ACACAC',
+    grayscale04: '#E2E2E2',
+    grayscale05: '#F5F5F5',
+    grayscale06: '#F9F9F9',
     white: '#FFFFFF',
 }
