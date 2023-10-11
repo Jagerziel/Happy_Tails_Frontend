4a386dc38a21be6d2d4887e73a65d3f1c75fd086
Jagerziel <fromnytosg@gmail.com>
2023-10-11 14:28:55 -0400

    added test data

--- src/data/testingData/testingData.js
+++ src/data/testingData/testingData.js
@@ -1,8 +1,58 @@
-const userData = {
-    
-}
+const userData = [
+    {
+        uid: 1,
+        first_name: "Clark",
+        last_name: "Kent",
+        email: "notsuperman@justiceleague.com",
+        phone: "7777777777",
+        password: "kryptonite",
+        address: "1 Justice Way",
+        state: "New York",
+        city: "Brooklyn",
+        zip: "11229",
+        ec_name: "Lois",
+        ec_phone: "Lane",
+        image: "https://images.unsplash.com/photo-1532202802379-df93d543bac3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80"
+    }
+]
 
+const petData = [
+    {
+        uid: 1,
+        name: "Lily",
+        type: "Dog",
+        dob: "2016/09/13",
+        breed: "Pitbull-mix",
+        image: "",
+        sex: "F",
+        weight: "60 lbs",
+        allergies: "none",
+        medications: "Dasequin",
+        laboratory: "n/a",
+        microchip: "Yes",
+        visit_history: "See appointments",
+        primary_color: "Brown",
+        notes: "She's amazing",
+        primary_vet: "Vet List",
+    }
+]
 
-const appointmentData = {
+const vaccinationData = [
+    {
+        uid: 1,
+        vaccine: "Rabies",
+        last_vaccine_date: "2023/09/26",
+        expiration_date: "2024/09/26",
+    }
+]
 
-}
+const appointmentData = [
+    {
+        uid: 1,
+        type: "Vaccination Appointment",
+        date: "2023/11/10",
+        time: "2:00 PM",
+        status: "Confirmed",
+        notes: "Update vaccination for the next year",
+    }
+]
