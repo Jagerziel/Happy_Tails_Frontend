fc103608c4357cefcfe7bc29f5ebcb46be693ede
Jagerziel <fromnytosg@gmail.com>
2023-10-17 10:27:50 -0400

    Conditionally style border of textInputField

--- src/components/shared/TextInputField.js
+++ src/components/shared/TextInputField.js
@@ -1,5 +1,5 @@
 // Import React
-import React from 'react';
+import React, { useState } from 'react';
 import { TextInput, TouchableOpacity, StyleSheet, Text, View } from 'react-native';
 // Import Constants
 import { colors } from '../../constants/colorPalette.js';
@@ -7,14 +7,30 @@ import { scale_H, scale_V, scale_mod } from '../../data/functions/normalizeScali
 import { styleMaster } from '../../constants/stylesMaster.js';
 
 function TextInputField( { name, placeholder, handleTextInput } ) {
+    const [ focused, setFocused ] = useState(false)
+    
+    function handleFocus() {
+        setFocused(focus => !focus)
+    }
+
     return (
-        <View style={styles.container}>
+        <View style={[
+            styles.container,
+            { borderColor: focused ? colors.primaryFade : colors.grayscale03 } // Conditionally style border
+            ]}
+        >
             <TextInput 
                 type='text'
-                style={[styleMaster.defaultFont, styles.textInput]}
+                style={[
+                    styleMaster.defaultFont, 
+                    styles.textInput,
+                    
+                ]}
                 placeholder={placeholder}
                 placeholderTextColor={colors.grayscale03}
                 onChangeText={text => handleTextInput(name, text)}
+                onFocus={() => handleFocus()}
+                onBlur={() => handleFocus()}
             />
         </View>
     );
@@ -24,8 +40,7 @@ export default TextInputField;
 
 const styles = StyleSheet.create({
     container: {
-        borderWidth: 2,
-        borderColor: colors.grayscale03,
+        borderWidth: scale_mod(2),
         width: scale_mod(328),
         aspectRatio: 6.83/1,
         display: 'flex',
