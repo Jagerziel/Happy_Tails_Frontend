b88a3bfc214d9bf5052e851e09c8a8038dac1a36
Jagerziel <fromnytosg@gmail.com>
2023-10-18 12:00:29 -0400

    updated addPetScreens 2 to 9

---
+++ src/screens/addPet/addPet02Screen.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { useNavigation, useRoute } from "@react-navigation/native";
+
+// Import Constants
+import { styleMaster } from "../../constants/stylesMaster.js";
+import { colors } from "../../constants/colorPalette.js";
+import { scale_H, scale_V, scale_mod } from "../../data/functions/normalizeScaling.js";
+
+// Import Components
+import LoginScreenButton from "../../components/shared/LoginScreenButton.js";
+import TextInputField from "../../components/shared/TextInputField.js";
+
+function AddPet02Screen( { route, navigation } ) {
+  
+  return (
+    <SafeAreaView style={[styles.container, styleMaster.parent]}>
+      <View style={[styleMaster.subParent]}>
+        <Text>AddPet Screen 02</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+export default AddPet02Screen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#eee",
+  },
+});
