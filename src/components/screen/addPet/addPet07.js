7b38e7f1133d5e949c2c6619a0cc3e6d84c569c1
Jagerziel <fromnytosg@gmail.com>
2023-10-18 11:35:49 -0400

    Added shell code for addPet02 thru addPet09

---
+++ src/components/screen/addPet/addPet07.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { useNavigation, useRoute } from "@react-navigation/native";
+
+// Import Constants
+import { styleMaster } from "../constants/stylesMaster.js";
+import { colors } from "../../../constants/colorPalette.js";
+import { scale_H, scale_V, scale_mod } from "../../../data/functions/normalizeScaling.js";
+
+// Import Components
+import LoginScreenButton from "../../shared/LoginScreenButton.js";
+import TextInputField from '../../shared/TextInputField.js'
+
+function AddPet07( { route, navigation } ) {
+  // Navigation
+  const navigation = useNavigation();
+  const route = useRoute();
+  
+  return (
+    <SafeAreaView style={[styles.container, styleMaster.parent]}>
+      <View style={[styleMaster.subParent]}>
+        <Text>AddPet Screen 07</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+export default AddPet07;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#eee",
+  },
+});
