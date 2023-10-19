95d56ee5cbe09f19a4b5a4104d04ab6b15c524d4
Jagerziel <fromnytosg@gmail.com>
2023-10-19 15:17:07 -0400

    Included base code for toggle button

--- src/screens/SettingsScreen.js
+++ src/screens/SettingsScreen.js
@@ -1,6 +1,12 @@
+// Import React
 import React from "react";
-import { SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { SafeAreaView, ScrollView, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { useNavigation, useRoute } from "@react-navigation/native";
+
+// Import Components
 import Navigation from "../components/shared/Navigation";
+import StaticInputFieldArrow from "../components/shared/StaticInputFieldArrow.js";
+
 
 // Import Constants
 import { styleMaster } from "../constants/stylesMaster.js";
@@ -8,17 +14,55 @@ import { scale_H, scale_V, scale_mod } from "../data/functions/normalizeScaling.
 import { colors } from "../constants/colorPalette.js";
 
 function SettingsScreen(props) {
+  // Navigation
+  const navigation = useNavigation();
+  const route = useRoute();
+
+  function handleLogout () {
+    /* 
+    **************************************************
+      ERASE STORED DATA
+    **************************************************
+    */
+    navigation.navigate("LoginScreen")
+    console.log('logged out')
+  }
+
+  function arrowNext ( path ) {
+    console.log(`${path} button pressed`)
+  }
+
+
   return (
     <SafeAreaView style={[styleMaster.parent, styles.container]}>
-      <View style={[styleMaster.subParent]}>
+      <ScrollView style={[styleMaster.subParent]}>
         <View style={styles.headerContainer}>
           <Text style={[styleMaster.defaultFont, styles.headerText]}>Settings</Text>
-          <Text style={[styleMaster.defaultFont, styles.logoutText]}>Log Out</Text>
-
-
+          <TouchableOpacity onPress={() => handleLogout()}>
+            <Text style={[styleMaster.defaultFont, styles.logoutText]}>Log Out</Text>
+          </TouchableOpacity>
+        </View>
+        <Text style={[styleMaster.defaultFont, styles.title]}>General</Text>
+        <View style={styles.staticInputContainer}>
+          <StaticInputFieldArrow name={"PIN"} arrowNext={() => arrowNext('PIN')} path={'PIN'}/>
         </View>
 
-      </View>
+        <Text style={[styleMaster.defaultFont, styles.title]}>User Details</Text>
+        <View style={styles.staticInputContainer}>
+          <StaticInputFieldArrow name={"User info"} arrowNext={() => arrowNext('User Info')} path={'User Info'}/>
+        </View>
+        <View style={styles.staticInputContainer}>
+          <StaticInputFieldArrow name={"Invoices"} arrowNext={() => arrowNext('Invoices')} path={'Invoices'}/>
+        </View>
+        <View style={styles.staticInputContainer}>
+          <StaticInputFieldArrow name={"Payment methods"} arrowNext={() => arrowNext('Payment methods')} path={'Payment methods'}/>
+        </View>
+        <View style={styles.staticInputContainer}>
+          <StaticInputFieldArrow name={"Emergency Contact"} arrowNext={() => arrowNext('Emergency Contact')} path={'Emergency Contact'}/>
+        </View>
+
+
+      </ScrollView>
       <Navigation />
     </SafeAreaView>
   );
@@ -37,15 +81,25 @@ const styles = StyleSheet.create({
     display: 'flex',
     flexDirection: "row",
     alignItems: 'center',
-    justifyContent: "space-between"
+    justifyContent: "space-between",
+    paddingBottom: scale_mod(4),
   },
   headerText: {
     fontSize: scale_V(32),
     fontFamily: "RalewayBold",
   },
   logoutText: {
-    fontFamily: "RobotoThin",
+    fontFamily: "RobotoLight",
     color: colors.black,
     paddingRight: scale_mod(7),
+  },
+  title: {
+    fontSize: scale_V(21),
+    fontFamily: "RalewayBold",
+    paddingTop: scale_mod(28),
+    paddingBottom: scale_mod(8),
+  },
+  staticInputContainer: {
+    paddingTop: scale_mod(8),
   }
 });
