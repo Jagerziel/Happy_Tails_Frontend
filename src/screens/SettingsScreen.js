773d35f492f139a3fbc78a5f5bfbf226eaa9bed9
Jagerziel <fromnytosg@gmail.com>
2023-10-19 14:46:15 -0400

    Starting Settings Screen

--- src/screens/SettingsScreen.js
+++ src/screens/SettingsScreen.js
@@ -1,15 +1,23 @@
 import React from "react";
-import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import Navigation from "../components/shared/Navigation";
 
 // Import Constants
 import { styleMaster } from "../constants/stylesMaster.js";
+import { scale_H, scale_V, scale_mod } from "../data/functions/normalizeScaling.js";
+import { colors } from "../constants/colorPalette.js";
 
 function SettingsScreen(props) {
   return (
-    <SafeAreaView style={[styles.container, styleMaster.parent]}>
+    <SafeAreaView style={[styleMaster.parent, styles.container]}>
       <View style={[styleMaster.subParent]}>
-        <Text>This is the Settings Screen</Text>
+        <View style={styles.headerContainer}>
+          <Text style={[styleMaster.defaultFont, styles.headerText]}>Settings</Text>
+          <Text style={[styleMaster.defaultFont, styles.logoutText]}>Log Out</Text>
+
+
+        </View>
+
       </View>
       <Navigation />
     </SafeAreaView>
@@ -25,4 +33,19 @@ const styles = StyleSheet.create({
     // alignItems: "center",
     // justifyContent: "center",
   },
+  headerContainer: {
+    display: 'flex',
+    flexDirection: "row",
+    alignItems: 'center',
+    justifyContent: "space-between"
+  },
+  headerText: {
+    fontSize: scale_V(32),
+    fontFamily: "RalewayBold",
+  },
+  logoutText: {
+    fontFamily: "RobotoThin",
+    color: colors.black,
+    paddingRight: scale_mod(7),
+  }
 });
