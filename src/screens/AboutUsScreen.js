36b6d51d915a4d651b6ba7cb558dcb9f0f594ae9
Jagerziel <fromnytosg@gmail.com>
2023-10-19 16:25:56 -0400

    Updated AboutUs Screen

--- src/screens/AboutUsScreen.js
+++ src/screens/AboutUsScreen.js
@@ -1,16 +1,50 @@
-import React from "react";
-import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+// Import React
+import React, { useState } from "react";
+import { Alert, SafeAreaView, ScrollView, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+
+// Import Components
 import Navigation from "../components/shared/Navigation";
+import LoginScreenButton from "../components/shared/LoginScreenButton.js";
 
 // Import Constants
 import { styleMaster } from "../constants/stylesMaster.js";
+import { scale_H, scale_V, scale_mod } from "../data/functions/normalizeScaling.js";
+import { colors } from "../constants/colorPalette.js";
 
 function AboutUsScreen(props) {
+  function handlePress(target) {
+    Alert.alert(`Under Construction`,`Start a New Chat feature coming soon!`)
+    console.log(`${target} button pressed`);
+  }
+
+
   return (
-    <SafeAreaView style={[styles.container, styleMaster.parent]}>
-      <View style={[styleMaster.subParent]}>
-        <Text>This is the About Us Screen</Text>
-      </View>
+    <SafeAreaView style={[styleMaster.parent, styles.container]}>
+      <ScrollView style={[styleMaster.subParent]}>
+        <View style={styles.headerContainer}>
+          <Text style={[styleMaster.defaultFont, styles.headerText]}>About Us</Text>
+        </View>
+        <Text style={[styleMaster.defaultFont, styles.title]}>Our Address</Text>
+        <View>
+          <Text style={[styleMaster.defaultFont, styles.text]}>4140 Parker Rd.</Text>
+          <Text style={[styleMaster.defaultFont, styles.text]}>Allentown, New Mexico 31134</Text>
+        </View>
+        <Text style={[styleMaster.defaultFont, styles.title]}>Phone Number</Text>
+        <View>
+          <Text style={[styleMaster.defaultFont, styles.text]}>(808) 555-0111</Text>
+        </View>
+        <Text style={[styleMaster.defaultFont, styles.title]}>Email</Text>
+        <View>
+          <Text style={[styleMaster.defaultFont, styles.text]}>bill.sanders@example.com</Text>
+        </View>
+        <Text style={[styleMaster.defaultFont, styles.title]}>Do you have a question?</Text>
+        <Text style={[styleMaster.defaultFont, styles.title, { paddingTop: 0, paddingBottom: scale_mod(32)}]}>Ask it right now!</Text>
+        <LoginScreenButton 
+          text={"Start a New chat"}
+          handlePress={() => handlePress("Start a New Chat")}
+        />
+
+      </ScrollView>
       <Navigation />
     </SafeAreaView>
   );
@@ -22,5 +56,27 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#eee",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  headerContainer: {
+    display: 'flex',
+    flexDirection: "row",
+    alignItems: 'center',
+    justifyContent: "space-between",
+    paddingBottom: scale_mod(4),
+  },
+  headerText: {
+    fontSize: scale_V(32),
+    fontFamily: "RalewayBold",
+  },
+  title: {
+    fontSize: scale_V(21),
+    fontFamily: "RalewayBold",
+    paddingTop: scale_mod(28),
+    paddingBottom: scale_mod(8),
+  },
+  text: {
+
   },
 });
